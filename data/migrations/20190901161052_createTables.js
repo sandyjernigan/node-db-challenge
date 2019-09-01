7d9aeed36d990aa13454fcdb49b2a6d8b18615f1
Sandy <sandy.jernigan@gmail.com>
2019-09-01 18:06:56 -0400

    create table for projects

--- data/migrations/20190901161052_createTables.js
+++ data/migrations/20190901161052_createTables.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  // A project is what needs to be done.
+  .createTable('projects', tbl => {
+    // a unique Id.
+    tbl.increments();
+    // a name. This column is required.
+    tbl.string('project_name').notNullable().unique();
+    // a description
+    tbl.text('project_description');
+    // a boolean that indicates if the project has been completed. 
+    // This column cannot be NULL, the default value should be false.
+    tbl.boolean('project_completed').notNullable();
+  })
+
+   
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('table');
 };
