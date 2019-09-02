20d25f9445470336309930fa93de16c05913dab6
Sandy <sandy.jernigan@gmail.com>
2019-09-01 20:20:54 -0400

    resources seed file

--- data/seeds/02-resources.js
+++ data/seeds/02-resources.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+  return knex('resources').insert([   
+    {
+      resource_name: 'Lambda Student',
+      resource_description: 'a soon to be hired developer'
+     },
+    {
+      resource_name: 'MacBook Pro #1',
+      resource_description: 'an overly expensive laptop computer'
+    }
+  ]);
 };
