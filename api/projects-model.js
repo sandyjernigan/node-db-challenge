9e67895326ebc240a363d80c10d45b611e017017
Sandy <sandy.jernigan@gmail.com>
2019-09-01 20:56:05 -0400

    added db-config and model  boiler plate and router boiler plate

---
+++ api/projects-model.js
@@ -0,0 +1,27 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  
+};
+
+//#region - CREATE
+
+// function add
+
+//#endregion
+
+//#region - READ
+
+//#endregion
+
+//#region - UPDATE
+
+//update
+
+//#endregion
+
+//#region - DELETE
+
+// remove
+
+//#endregion
---
+++ api/projects-router.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const Recipes = require('./-model.js');
+
+const router = express.Router();
+
+//#region - CREATE
+router.post('/', async (req, res) => {});
+
+//#endregion
+
+//#region - READ
+
+// GET
+router.get('/', async (req, res) => {
+  try {
+  } catch (err) {
+  }
+});
+
+// GET by id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+  } catch (err) {
+  }
+});
+
+//#endregion
+
+//#region - UPDATE
+router.put('/:id', async (req, res) => {});
+
+//#endregion
+
+//#region - DELETE
+router.delete('/:id', async (req, res) => {});
+//#endregion
+
+//#region - Custom Middleware
+//#endregion
+
+module.exports = router;
